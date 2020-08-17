0ee76afbbcff8613b760b83a4bdd8828b0c4ab26
Dario Seidl <darioseidl@fastmail.fm>
2020-08-17 12:58:00 +0200

    Return 200 without a body for favicon.ico requests

---
+++ src/main/kotlin/at/rechnerherz/example/web/FaviconController.kt
@@ -0,0 +1,19 @@
+package at.rechnerherz.example.web
+
+import at.rechnerherz.example.config.FAVICON
+import at.rechnerherz.example.config.aop.NoProfiling
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class FaviconController {
+
+    /**
+     * Return 200 without a body for favicon.ico requests.
+     */
+    @GetMapping(FAVICON)
+    @ResponseBody
+    @NoProfiling
+    fun noFavicon() {}
+}
