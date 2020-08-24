23e2b686394bf8553c8ec6201d4cbcf740bf38b0
Dario Seidl <darioseidl@fastmail.fm>
2020-08-24 17:45:26 +0200

    Improve locale/country methods

--- src/main/kotlin/at/rechnerherz/example/util/Locale.kt
+++ src/main/kotlin/at/rechnerherz/example/util/Locale.kt
@@ -1,33 +1,53 @@
 package at.rechnerherz.example.util
 
 import org.springframework.context.MessageSource
+import java.time.format.DateTimeFormatter
 import java.util.*
 
-fun localeFromAlpha2CountryCode(alpha2CountryCode: String): Locale? =
-    Locale.getAvailableLocales().find { it.country == alpha2CountryCode }
-
-fun localeFromAlpha3CountryCode(alpha3CountryCode: String): Locale? =
-    Locale.getAvailableLocales().find {
-        try {
-            it.isO3Country == alpha3CountryCode
-        } catch (e: MissingResourceException) {
-            false
-        }
+/**
+ * Return the ISO alpha-3 country code from a locale, or null if not available
+ * (instead of throwing a [MissingResourceException]).
+ */
+val Locale.iso3CountryOrNull
+    get() = try {
+        isO3Country
+    } catch (e: MissingResourceException) {
+        null
     }
 
-fun alpha2ToAlpha3CountryCode(alpha2CountryCode: String): String? =
-    localeFromAlpha2CountryCode(alpha2CountryCode)?.isO3Country
+/**
+ * Return a [Locale] for the given ISO alpha-2 country code, or null if not available.
+ */
+fun localeFromAlpha2CountryCode(alpha2CountryCode: String?): Locale? =
+    if (alpha2CountryCode.isNullOrBlank()) null else Locale.getAvailableLocales().find { it.country == alpha2CountryCode }
 
-fun alpha3ToAlpha2CountryCode(alpha3CountryCode: String): String? =
+/**
+ * Return a [Locale] for the given ISO alpha-3 country code, or null if not available.
+ */
+fun localeFromAlpha3CountryCode(alpha3CountryCode: String?): Locale? =
+    if (alpha3CountryCode.isNullOrBlank()) null else Locale.getAvailableLocales().find { it.iso3CountryOrNull == alpha3CountryCode }
+
+/**
+ * Return the ISO alpha-3 country code for the given ISO alpha-2 country code, or null if not available.
+ */
+fun alpha2ToAlpha3CountryCode(alpha2CountryCode: String?): String? =
+    localeFromAlpha2CountryCode(alpha2CountryCode)?.iso3CountryOrNull
+
+/**
+ * Return the ISO alpha-2 country code for the given ISO alpha-3 country code, or null if not available.
+ */
+fun alpha3ToAlpha2CountryCode(alpha3CountryCode: String?): String? =
     localeFromAlpha3CountryCode(alpha3CountryCode)?.country
 
 /**
  * Parse a country code from an unknown format.
  *
- * Trim the input then try
+ * Trim the input, convert to upper case, then try
+ *
  *  - ISO alpha-3
  *  - ISO alpha-2
  *  - single-letter licence plate numbers of European countries.
+ *
  * Returns an ISO alpha-3 code, or null if nothing matched.
  *
  * [alpha-2](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2),
@@ -35,10 +55,10 @@ fun alpha3ToAlpha2CountryCode(alpha3CountryCode: String): String? =
  * [Vehicle registration plates of Europe](https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Europe#European_Union)
  */
 fun parseToAlpha3CountryCode(countryCode: String?): String? =
-    countryCode?.trim()?.let {
+    countryCode?.trim()?.toUpperCase()?.let {
         when {
-            Regex("[A-Z]{3}").matches(it) -> localeFromAlpha2CountryCode(it)?.isO3Country
-            Regex("[A-Z]{2}").matches(it) -> alpha2ToAlpha3CountryCode(it)
+            Regex("[A-Z]{3}").matches(it) -> localeFromAlpha3CountryCode(it)?.iso3CountryOrNull
+            Regex("[A-Z]{2}").matches(it) -> localeFromAlpha2CountryCode(it)?.iso3CountryOrNull
             it === "A" -> "AUT"
             it === "B" -> "BEL"
             it === "D" -> "DEU"
@@ -59,11 +79,17 @@ fun parseToAlpha3CountryCode(countryCode: String?): String? =
 /**
  * Returns the display name of the country for the given alpha3 country code in the language of the given locale.
  */
-fun countryDisplayName(alpha3CountryCode: String, locale: Locale): String? =
+fun countryDisplayName(alpha3CountryCode: String?, locale: Locale): String? =
     localeFromAlpha3CountryCode(alpha3CountryCode)?.getDisplayCountry(locale)
 
 /**
- * Convenience wrapper for [MessageSource.getMessage] accepting vararg arguments.
+ * Convenience wrapper for [MessageSource.message] accepting vararg arguments.
  */
-fun MessageSource.getMessage(locale: Locale, code: String, vararg args: Any): String =
+fun MessageSource.message(locale: Locale, code: String, vararg args: Any): String =
     getMessage(code, args, locale)
+
+/**
+ * Convenience method to get a [DateTimeFormatter] from a format from a [MessageSource].
+ */
+fun MessageSource.formatter(locale: Locale, code: String, vararg args: Any): DateTimeFormatter =
+    DateTimeFormatter.ofPattern(message(locale, code, args)).withLocale(locale)
